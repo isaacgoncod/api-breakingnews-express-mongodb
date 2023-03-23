d33c0230fe5372a09c236fb5f267886af431c72a
isaacgoncod <isaacgoncod@gmail.com>
2023-03-23 17:31:02 -0300

    render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import express from "express";
-import connectDatabase from "./src/database/conn.js";
+import connectDatabase from "./database/conn.js";
 import dotenv from "dotenv";
 
-import userRoute from "./src/routes/user.route.js";
-import authRoute from "./src/routes/auth.route.js";
-import newsRoute from "./src/routes/news.route.js";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+import newsRoute from "./routes/news.route.js";
+import swaggerRoute from "./routes/swagger.route.cjs";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 connectDatabase();
 
@@ -18,5 +19,6 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute);
 
 app.listen(port, () => console.log(`Server rodando na porta: ${port}`));
--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -22,7 +22,7 @@ router.get("/top", topNews);
 router.get("/search", searchByTitle);
 router.get("/byUser", authMiddleware, byUser);
 router.get("/:id", authMiddleware, findById);
-router.patch("/:id", authMiddleware, update);
+router.patch("/update/:id", authMiddleware, update);
 router.delete("/:id", authMiddleware, erase);
 router.patch("/like/:id", authMiddleware, likeNews);
 router.patch("/comment/:id", authMiddleware, addComment);
