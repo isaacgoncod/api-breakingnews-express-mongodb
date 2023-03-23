bae16163961a1143d820bfe8a293423056eebd51
isaacgoncod <isaacgoncod@gmail.com>
2023-03-23 17:52:28 -0300

    install cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDatabase from "./database/conn.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
@@ -10,11 +11,12 @@ import swaggerRoute from "./routes/swagger.route.cjs";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 connectDatabase();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
